2f3177ad706273bcd901eefc4ee59424750d360a
Vanterric <46641370+Vanterric@users.noreply.github.com>
2023-01-14 18:38:55 +0000

    Deploying to gh-pages from @ YitingLiu97/MITHack2023@4477c0cc066e5843ab57ce6a44c637ca51dd8fa9 🚀

--- PeerTest-bundle.js
+++ PeerTest-bundle.js
@@ -419,6 +419,25 @@ WL.registerComponent('emoji_particle_shooter', {
     }
 });
 
+WL.registerComponent('gun_trigger_manager', {
+    trigger: {type: WL.Type.Object, default: null},
+    particleShooter: {type: WL.Type.Object, default: null},
+}, {
+    init: function() {
+
+    },
+    start: function() {
+        const particleShooterFunction = this.particleShooter.getComponent("emoji_particle_shooter")
+        
+        console.log(particleShooterFunction.active)
+        this.trigger.getComponent('cursor-target').addDownFunction(function(){particleShooterFunction.active = true;});
+        this.trigger.getComponent('cursor-target').addUpFunction(function(){particleShooterFunction.objects.forEach(object => {
+            object.active = false;
+        }); particleShooterFunction.active = false});
+    },
+    
+});
+
 /**
 Very simple mesh particles system
 
