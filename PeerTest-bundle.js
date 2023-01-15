49ce9d1a1cecddc09939abc92ae10fba857e9f72
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2023-01-15 01:12:51 +0000

    Deploying to gh-pages from @ YitingLiu97/MITHack2023@bcf965bc32cc22f2ee234019e52a68ec213fe23a 🚀

--- PeerTest-bundle.js
+++ PeerTest-bundle.js
@@ -333,7 +333,38 @@ WL.registerComponent("controller-teleport-component", {
     }
 });
 
-WL.registerComponent('emojiMsg', {
+WL.registerComponent('emojiReceiver', {
+    peerComponent: {type: WL.Type.Object},
+    emojiSpawner: {type:WL.Type.Object},
+}, {
+    init: function() {
+        console.log('init() with param', this.param);
+    },
+    start: function() {
+        let peerManager=  this.peerComponent.getComponent('peer-manager');
+        let emoSpawner=  this.peerComponent.emojiSpawner('emoji_particle_shooter');
+        
+        // send message when you are interacting in XR 
+        //peerManager.sendPackage("emoji","heart");
+        
+        peerManager.addNetworkDataRecievedCallback("emoji",()=>{
+            console.log("data received, needs to spawn");
+            emoSpawner.spawn();
+        });  
+        
+        peerManager.removeNetworkDataRecievedCallback("emoji",()=>{
+            console.log("data received, needs to REMOVE");
+        });
+        
+        
+        console.log('start() with param', this.param);
+    },
+    update: function(dt) {
+        console.log('update() with delta time', dt);
+    },
+});
+
+WL.registerComponent('emojiSender', {
     message: {type: WL.Type.String},
     peerComponent: {type: WL.Type.Object},
     emojiSpawner: {type:WL.Type.Object},
@@ -344,15 +375,15 @@ WL.registerComponent('emojiMsg', {
     start: function() {
         let peerManager=  this.peerComponent.getComponent('peer-manager');
         let emoSpawner=  this.peerComponent.emojiSpawner('emoji_particle_shooter');
+        
+        // send message when you are interacting in XR 
         peerManager.sendPackage("emoji","heart");
         
-
-
-
-        peerManager.addNetworkDataRecievedCallback("emoji",()=>{
-            console.log("data received");
-            
-        })
+      /*  peerManager.addNetworkDataRecievedCallback("emoji",()=>{
+            console.log("data received, needs to spawn");
+            emoSpawner.spawn();
+        })*/
+        
         console.log('start() with param', this.param);
     },
     update: function(dt) {
