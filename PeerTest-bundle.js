41be7571f4de532866c06b2332fe00c0d94b8a53
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2023-01-15 03:55:32 +0000

    Deploying to gh-pages from @ YitingLiu97/MITHack2023@1e9af6ccf32772fdda707117750f7d8d84f167e6 🚀

--- PeerTest-bundle.js
+++ PeerTest-bundle.js
@@ -24,50 +24,68 @@
 WL.registerComponent('changeColor', {
     buttonMeshObject: {type: WL.Type.Object},
     hoverMaterial: {type: WL.Type.Material},
+    peerComponent: {type: WL.Type.Object},
+
 }, {
     start: function () {
         this.mesh = this.buttonMeshObject.getComponent('mesh');
         this.defaultMaterial = this.mesh.material;
+        this.peerManager = this.peerComponent.getComponent('peer-manager');
 
         this.target = this.object.getComponent('cursor-target') || this.object.addComponent('cursor-target');
         this.target.addClickFunction(this.onClick.bind(this));
+        
+       this.peerManager.addNetworkDataRecievedCallback("change-color", (d) => {
+            console.log("change color received", d);
+            this.setColor(d.color);
+            //emoSpawner.spawn();
+        });
+        
         this.target.addDownFunction(this.onDown.bind(this));
 
         this.target.addHoverFunction(this.onHover.bind(this));
         this.target.addUnHoverFunction(this.onUnHover.bind(this));
-         this.target.addUpFunction(this.onUp.bind(this));
+        this.target.addUpFunction(this.onUp.bind(this));
 
-      /*  this.soundClick = this.object.addComponent('howler-audio-source', {src: 'sfx/click.wav', spatial: true});
-        this.soundUnClick = this.object.addComponent('howler-audio-source', {src: 'sfx/unclick.wav', spatial: true});
-*/
+        /*  this.soundClick = this.object.addComponent('howler-audio-source', {src: 'sfx/click.wav', spatial: true});
+          this.soundUnClick = this.object.addComponent('howler-audio-source', {src: 'sfx/unclick.wav', spatial: true});
+  */
     },
 
+    setColor: function (color) {
+        let mat = this.defaultMaterial.clone();
+        mat.ambientColor = color;
+        this.mesh.material = mat;
+
+    },
     onClick: function (_, cursor) {
         const c = Math.random() * 1;
         const d = c * Math.random();
-        let mat = this.defaultMaterial.clone();
-        mat.ambientColor = [c, d, c / d, 1];
-        this.mesh.material = mat;
+        let color = [c, d, c / d, 1];
+        console.log("set color before ", color);
+        this.setColor(color);
+        console.log("set color ", color);
+        this.peerManager.sendPackageImmediately("change-color", {color: color});
         if (cursor.type == 'finger-cursor') {
             this.onDown(_, cursor);
         }
     },
     onDown: function (_, cursor) {
-      //  this.soundClick.play();
+        //  this.soundClick.play();
         this.buttonMeshObject.translate([0.0, 0.0, 0.02]);
         this.hapticFeedback(cursor.object, 1.0, 20);
-    }, 
-    
+    },
+
     hapticFeedback: function (object, strength, duration) {
         const input = object.getComponent('input');
         if (input && input.xrInputSource) {
             const gamepad = input.xrInputSource.gamepad;
             if (gamepad && gamepad.hapticActuators) gamepad.hapticActuators[0].pulse(strength, duration);
-           
+
         }
     },
-    
-    
+
+
     //grip to mute and unmute
     onUnHover: function (_, cursor) {
         this.mesh.material = this.defaultMaterial;
@@ -88,7 +106,7 @@ WL.registerComponent('changeColor', {
     },
 
     onUp: function (_, cursor) {
-      //  this.soundUnClick.play();
+        //  this.soundUnClick.play();
         this.buttonMeshObject.translate([0.0, 0.0, -0.02]);
         this.hapticFeedback(cursor.object, 0.7, 20);
 
@@ -347,12 +365,13 @@ WL.registerComponent('emojiReceiver', {
         
         peerManager.addNetworkDataRecievedCallback("emoji-start",(d)=>{
             console.log("emoji start received",d);
-            //emoSpawner.spawn();
+            emoSpawner.spawn();
         });
         peerManager.addNetworkDataRecievedCallback("emoji-stop",(d)=>{
             console.log("emoji stop received",d);
             //emoSpawner.spawn();
         });
+       
 
         console.log('start() with param', this.param);
     },
@@ -506,6 +525,7 @@ WL.registerComponent('emoji_particle_shooter', {
         const origin = this.gun.getTranslationWorld([])
         
         obj.translate(origin);
+        console.log("spawning, translate to origin "+origin );
 
         this.velocities[index][0] = 0;
         this.velocities[index][1] = 0;
@@ -756,15 +776,20 @@ WL.registerComponent('network-buttons', {
 
         this.pm.peer.on('error', (e) => {
             console.error(e);
-            this.pm.join();
-            console.log("trying to connect as guest");
+            setTimeout(()=>{
+                this.pm.join();
+                console.log("trying to connect as guest");
 
-            this.pm.peer.on('error', (e) => {
-                console.error(e);
-                this.pm.host();
-                console.log("trying to host again");
-
-            });
+                this.pm.peer.on('error', (e) => {
+                    console.error(e);
+                    setTimeout(()=> {
+                        this.pm.host();
+                        console.log("trying to host again");
+                    },100);
+                });
+                
+            },100);
+            
             
         });
 
@@ -824,7 +849,7 @@ WL.registerComponent("peer-manager", {
   // Host functions
   //
   host: function() {
-    this.peer = new Peer(this.serverId);
+    this.peer = new Peer(this.serverId,{debug:0});
     this.peer.on("open", this._onHostOpen.bind(this));
     this.peer.on("connection", this._onHostConnected.bind(this));
     this.peer.on("disconnected", this._onDisconnected.bind(this));
