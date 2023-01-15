3659a1ee693fdfedcebc962d384ccdc3f9895bf7
Vanterric <46641370+Vanterric@users.noreply.github.com>
2023-01-15 01:25:17 +0000

    Deploying to gh-pages from @ YitingLiu97/MITHack2023@84e5ae34e37efb0634e43be62344071ffd218022 🚀

--- PeerTest-bundle.js
+++ PeerTest-bundle.js
@@ -1424,6 +1424,20 @@ WL.registerComponent('playbook-toggle', {
     }
 });
 
+WL.registerComponent('setEmoji', {
+    param: {type: WL.Type.Float, default: 1.0},
+}, {
+    init: function() {
+        console.log('init() with param', this.param);
+    },
+    start: function() {
+        console.log('start() with param', this.param);
+    },
+    update: function(dt) {
+        console.log('update() with delta time', dt);
+    },
+});
+
 WL.registerComponent('skybox', {
     skyMat: {type: WL.Type.Material},
     light1: {type: WL.Type.Object},
