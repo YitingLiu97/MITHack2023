80aff5a2daedb5fa3541c63032208c758a88674d
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2023-01-15 00:59:44 +0000

    Deploying to gh-pages from @ YitingLiu97/MITHack2023@31a4dae2b7e4279af174d4931739577aaa5ff145 🚀

--- PeerTest-bundle.js
+++ PeerTest-bundle.js
@@ -333,6 +333,33 @@ WL.registerComponent("controller-teleport-component", {
     }
 });
 
+WL.registerComponent('emojiMsg', {
+    message: {type: WL.Type.String},
+    peerComponent: {type: WL.Type.Object},
+    emojiSpawner: {type:WL.Type.Object},
+}, {
+    init: function() {
+        console.log('init() with param', this.param);
+    },
+    start: function() {
+        let peerManager=  this.peerComponent.getComponent('peer-manager');
+        let emoSpawner=  this.peerComponent.emojiSpawner('emoji_particle_shooter');
+        peerManager.sendPackage("emoji","heart");
+        
+
+
+
+        peerManager.addNetworkDataRecievedCallback("emoji",()=>{
+            console.log("data received");
+            
+        })
+        console.log('start() with param', this.param);
+    },
+    update: function(dt) {
+        console.log('update() with delta time', dt);
+    },
+});
+
 /**
 Very simple mesh particles system
 
@@ -1064,6 +1091,7 @@ WL.registerComponent("peer-manager", {
 
   sendPackage: function(key, data) {
     this.currentDataPackage[key] = data;
+    
   },
 
   sendPackageImmediately: function(key, data) {
